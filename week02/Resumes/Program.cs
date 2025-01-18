92f5ac77014101c0503c68d66da2d316c7fe16d0
charlessalva <charlesnephiibascosalva@gmail.com>
2025-01-18 23:26:49 +0800

    Created the classes for Job and Resume

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -1,9 +1,33 @@
 using System;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job();
+        {
+            job1._jobTitle = "Software Engineer";
+            job1._company = "Microsoft";
+            job1._startYear = 1999;
+            job1._endYear = 2010;
+        }
+
+        Job job2 = new Job();
+        {
+            job2._jobTitle = "Data Analyst";
+            job2._company = "Apple";
+            job2._startYear = 2003;
+            job2._endYear = 2016;
+        }
+
+        Resume resume1 = new Resume();
+        {
+            resume1._personName = "Charles Salva";
+            resume1._jobs.Add(job1);
+            resume1._jobs.Add(job2);
+        }
+
+        resume1.DisplayResume();
     }
 }
